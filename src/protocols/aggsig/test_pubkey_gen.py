e4bfb2aaec00c19566f3e609c45ad48e5c6212ed
Shalev Keren <shalev.ke@gmail.com>
2021-10-06 12:39:36 +0300

    b

--- src/protocols/aggsig/test_pubkey_gen.py
+++ src/protocols/aggsig/test_pubkey_gen.py
@@ -7,9 +7,6 @@ def main():
     # Generate a new random signing key
     signing_key = SigningKey('48ab347b2846f96b7bcd00bf985c52b83b92415c5c914bc1f3b09e186cf2b14f', encoder=HexEncoder)
 
-    # Sign a message with the signing key
-    signed_hex = signing_key.sign(b"Attack at Dawn", encoder=HexEncoder)
-
     # Obtain the verify key for a given signing key
     verify_key = signing_key.verify_key
 
